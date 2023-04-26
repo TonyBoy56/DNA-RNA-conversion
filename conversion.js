996b1bb29f433fa84b9188d5849dd929c8a7d85b
Antonio Cruz <antonio.cruz5612@gmail.com>
2023-04-26 14:54:46 -0700

    return RNA using .split().join() method.

--- conversion.js
+++ conversion.js
@@ -1,15 +1,8 @@
 const dna = "GGGCCCAAAATTTTTCCCA";
 
 function replaceToRNA(string) {
-    let rna = "";
-    for (let i = 0; i < string.length; i++) {
-        if (string[i] === "T") {
-            rna += "U";
-        } else {
-            rna += string[i];
-        }
-    }
-    console.log(rna);
+    // return statement here
+    console.log(string.split('T').join('U'));
 }
 
 replaceToRNA(dna);
